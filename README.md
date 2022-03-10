8d347077e7fbfdfa3a8e517b021c7765928eeffd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-10 15:51:14 -0500

    add screenshot

---
+++ README.md
@@ -0,0 +1,7 @@
+# cypress-lighthouse-example
+
+> Getting the Lighthouse performance metrics from a Cypress test
+
+![Test with Lighthouse audit](./images/pass.png)
+
+See [spec.js](./cypress/integration/spec.js), uses plugin [@cypress-audit/lighthouse](https://github.com/mfrachet/cypress-audit/tree/master/packages/lighthouse)
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,5 +2,22 @@
 
 it('loads fast enough', () => {
   cy.visit('/')
-  cy.lighthouse()
+  cy.lighthouse(
+    {
+      performance: 60,
+      accessibility: 90,
+      'best-practices': 80,
+      seo: 80,
+    },
+    {
+      formFactor: 'desktop',
+      screenEmulation: {
+        mobile: false,
+        disable: false,
+        width: Cypress.config('viewportWidth'),
+        height: Cypress.config('viewportHeight'),
+        deviceScaleRatio: 1,
+      },
+    },
+  )
 })
