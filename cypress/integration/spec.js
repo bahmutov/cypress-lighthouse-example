5659bb104a46ebc182eb3b0c89b07b107e708457
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-10 16:19:24 -0500

    custom message

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -4,7 +4,7 @@ it('loads fast enough', () => {
   cy.visit('/')
   cy.lighthouse(
     {
-      performance: 60,
+      performance: 50,
       accessibility: 90,
       'best-practices': 80,
       seo: 80,
@@ -20,4 +20,38 @@ it('loads fast enough', () => {
       },
     },
   )
+})
+
+it('shows the text report returned by from the plugins task', () => {
+  cy.visit('/')
+
+  const thresholds = {
+    performance: 50,
+    accessibility: 90,
+    'best-practices': 80,
+    seo: 80,
+  }
+  const opts = {
+    formFactor: 'desktop',
+    screenEmulation: {
+      mobile: false,
+      disable: false,
+      width: Cypress.config('viewportWidth'),
+      height: Cypress.config('viewportHeight'),
+      deviceScaleRatio: 1,
+    },
+  }
+  cy.url()
+    .then((url) => {
+      cy.task('lighthouse', {
+        url,
+        thresholds,
+        opts,
+      })
+    })
+    .then((report) => {
+      const { errors, results, txt } = report
+      // our custom code in the plugins file has summarized the report
+      cy.log(report.txt)
+    })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,6 +6,35 @@ module.exports = (on, config) => {
   })
 
   on('task', {
-    lighthouse: lighthouse(), // calling the function is important
+    async lighthouse(allOptions) {
+      let txt
+      // calling the function is important
+      const lighthouseTask = lighthouse((lighthouseReport) => {
+        let lighthouseScoreText = ''
+        let lighthouseResult = lighthouseReport?.lhr?.categories
+        let lighthousePerformance =
+          'Performance: ' + lighthouseResult?.performance?.score + '\n'
+        let lighthouseAccessibility =
+          'Accessibility: ' + lighthouseResult?.accessibility?.score + '\n'
+        let lighthouseBestPractices =
+          'Best Practices: ' +
+          lighthouseResult?.['best-practices']?.score +
+          '\n'
+        let lighthouseSEO = 'SEO: ' + lighthouseResult?.seo?.score + '\n'
+        lighthouseScoreText =
+          lighthousePerformance +
+          lighthouseAccessibility +
+          lighthouseBestPractices +
+          lighthouseSEO
+
+        console.log(lighthouseScoreText)
+        txt = lighthouseScoreText
+      })
+
+      const report = await lighthouseTask(allOptions)
+      // insert the text into the report returned the test
+      report.txt = txt
+      return report
+    },
   })
 }
